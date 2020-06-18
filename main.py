f3c4f52f631abb7a868beb324155aea98339d854
Piyush <mf08ps1@mail.wbs.ac.uk>
2020-06-18 19:45:31 +0100

    Add new tflite file

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import argparse
 import os
 import sys
 import math
-
+import numpy as np
 from tflite import Model
 
 import pdb as pdb
@@ -22,7 +22,38 @@ def main(model):
     fb = tfh.read()
     tfh.close()
 
-    print("Flatbuffer size = ")
+    model_bytes = bytearray(fb)
+    print("Model {} = {} bytes".format(model, len(fb)))
+
+    model = Model.Model.GetRootAsModel(model_bytes, 0)
+    subgraph = model.Subgraphs(0)
+    for i in range(subgraph.TensorsLength()):
+        t = subgraph.Tensors(i)
+        tens = {}
+        tens["id"]=i
+        tens["shape"]=t.ShapeAsNumpy()
+        tens["name"]=t.Name().decode("ascii")
+        #producer=None
+        #consumers=[]
+        tens["type"]=t.Type()
+        print(tens)
+        #pdb.set_trace()
+
+
+    for i in range(subgraph.OperatorsLength()):
+        op = subgraph.Operators(i)
+        assert op.OutputsLength() <= 1
+        has_output = op.OutputsLength() == 1
+        inputs = [j for j in op.InputsAsNumpy()]
+        assert len(inputs) > 0
+
+        #tflite_op = TFLiteOperator(id=i, output=tensors[op.Outputs(0)] if has_output else None, inputs=inputs)
+        #tflite_op.output.producer = tflite_op
+        #for t in inputs:
+        #    t.consumers.append(tflite_op)
+        #operators.append(tflite_op)
+
+
     pdb.set_trace()
 
 
